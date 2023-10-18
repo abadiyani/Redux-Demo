469075eab31f0e5ce9e20a6b0e42f56dea585969
Aakash Badiyani <badiyani.aakash@gmail.com>
2023-10-18 16:00:23 -0400

    Implemented store, subscriber, dispatcher and unsubscriber.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+//type
 const CAKE_ORDERED = "CAKE_ORDERED";
 
+//action creator
 const orderCake = () => {
     return {
         type: CAKE_ORDERED,
@@ -7,10 +12,12 @@ const orderCake = () => {
     }
 }
 
+//state
 const initialState = {
     numberOfCakes: 10
 }
 
+//reducer
 const reducer = (state = initialState, action) => {
     switch(action.type){
         case CAKE_ORDERED:
@@ -22,3 +29,23 @@ const reducer = (state = initialState, action) => {
             return state;
     }
 }
+
+//create store
+const store = createStore(reducer);
+
+//initial state
+console.log("Initial State: ", store.getState());
+
+//subscriber
+const unsubscribe = store.subscribe(() => {
+    console.log("Updated State: ", store.getState());
+});
+
+//dispatching actions
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+//unsubscribe
+unsubscribe();
+
